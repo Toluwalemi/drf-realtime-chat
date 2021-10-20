b7f7f6c1e44eb5ec82150cc9fea88e2595a673d4
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-20 23:01:14 +0100

    feature: register models in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import Store, Discount, Operator, Client, Conversation, Chat, Schedule
+
+admin.site.register(Store)
+admin.site.register(Discount)
+admin.site.register(Operator)
+admin.site.register(Client)
+admin.site.register(Conversation)
+admin.site.register(Chat)
+admin.site.register(Schedule)
