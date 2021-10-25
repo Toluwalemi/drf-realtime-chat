93312e1ef5c3dba14cde684d8a2cbb96e6061e41
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-25 20:56:35 +0100

    fix: change field name

--- api/serializers.py
+++ api/serializers.py
@@ -56,28 +56,25 @@ class ChatSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ConversationSerializer(serializers.HyperlinkedModelSerializer):
-    chats = ChatSerializer(many=True, read_only=True)
+    chats = ChatSerializer(many=True, read_only=True, source='conversation_chats')
     store_id = serializers.SlugRelatedField(queryset=Store.objects.all(), slug_field='pk', source='store')
     client_id = serializers.SlugRelatedField(queryset=Client.objects.all(), slug_field='pk', source='client')
     operator_id = serializers.SlugRelatedField(queryset=Operator.objects.all(), slug_field='pk', source='operator')
 
     class Meta:
         model = Conversation
-        fields = ('url', 'store_id', 'operator_id', 'client_id', 'status', 'chats')
+        fields = ('url', 'store_id', 'operator_id', 'client_id', 'status', 'chats',)
 
 
 class ChatConversationSerializer(serializers.ModelSerializer):
     conversation_id = serializers.SlugRelatedField(queryset=Conversation.objects.all(), slug_field='pk',
                                                    source='conversation')
 
-    # user_id = serializers.(source='chat_user.pk')
-    # user_id = serializers.SlugRelatedField(queryset=Discount.objects.all(), slug_field='discount_code',
-    #                                        write_only=True)
-
+    user_id = serializers.ReadOnlyField(source='chat_user.pk')
     discount_id = serializers.SlugRelatedField(queryset=Discount.objects.all(), slug_field='discount_code',
                                                write_only=True, source='discount')
 
     class Meta:
         model = Chat
-        fields = ('url', 'payload', 'chat_user', 'discount_id', 'conversation_id', 'created', 'status',)
+        fields = ('url', 'payload', 'user_id', 'discount_id', 'conversation_id', 'created', 'status',)
         read_only_fields = ('status',)
