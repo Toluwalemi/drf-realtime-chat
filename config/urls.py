036f4633b2dc5bdac8b890b9cf12edcbea8bd40f
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-26 13:46:14 +0100

    feature: add path for health check status

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from api.views import ping
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ping/', ping, name="ping"),
     path('api/', include('api.urls')),
 ]
